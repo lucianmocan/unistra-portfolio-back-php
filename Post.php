df5d4ef1d6926da08c9e8f983d50bd45e04209c5
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-04 00:40:05 +0200

    addUser function in Post class

--- Post.php
+++ Post.php
@@ -1,6 +1,8 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
 require_once 'Database.php';
+require_once 'Contact.php';
 
 class Post extends Database {
     public function __construct(){
@@ -16,6 +18,17 @@ class Post extends Database {
             email VARCHAR(255) NOT NULL,
             message TEXT NOT NULL
         )');
+    }
+
+    public function addUser(Contact $user){
+        $stmt = $this->pdo->prepare("INSERT INTO contactRequests (
+                             firstname, lastname, email, message) 
+                            VALUES (:firstName, :lastName, :email, :message)");
+        $stmt->bindValue(':firstName', $user->getFirstName());
+        $stmt->bindValue(':lastName', $user->getLastName());
+        $stmt->bindValue(':email', $user->getEmail());
+        $stmt->bindValue(':message', $user->getContent());
+        $stmt->execute();
     }
 
     // create 2 php files one which gets called by the httpClient et l'autre qui fait le traitement.
