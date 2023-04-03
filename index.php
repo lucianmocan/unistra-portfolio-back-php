2a78a7f15cf743b6c6f1aba41a79d5dab85a0edd
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-04 00:39:33 +0200

    to see if redirections work

--- index.php
+++ index.php
@@ -2,6 +2,20 @@
 header('Access-Control-Allow-Origin: *');
 header('Access-Control-Allow-Headers: *');
 
-$postdata = file_get_contents("php://input");
-echo json_encode($postdata);
-//header('Content-Type: application/json');
+
+//if (isset($_SERVER['REQUEST_URI'])){
+//    $url = $_SERVER['REQUEST_URI'];
+//    if (strcmp($url, "/addUser") == 0){
+//        header('Location: http://localhost:8000/addcontact.php');
+//    }
+//    if (strcmp($url, "/getUsers") == 0){
+//        header('Location: http://localhost:8000/getusers.php');
+//    }
+//}
+
+
+//$postdata = file_get_contents("php://input");
+//echo json_encode($postdata);
+
+//if (isset($_POST)) echo var_dump($_POST);
+//echo $_SERVER['QUERY_STRING'];
