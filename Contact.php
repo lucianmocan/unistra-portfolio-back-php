d146254868160251393897dc17606031d3f317e7
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-04 00:38:25 +0200

    constructor for Contact class email validation on backend

--- Contact.php
+++ Contact.php
@@ -1,11 +1,37 @@
 <?php
-header('Access-Control-Allow-Origin: *');
 class Contact
 {
     private string $firstName;
     private string $lastName;
     private string $email;
     private string $content;
+
+    function __construct($user){
+        $this->firstName = htmlspecialchars($user["firstName"]);
+        $this->lastName = htmlspecialchars($user["lastName"]);
+        try {
+            $this->email = $this->checkEmail($user["email"]);
+        }
+        catch (Exception $e) {
+            http_response_code(406);
+            echo json_encode(array("message" => "incorrect email format"));
+            echo "Connection error" . $e->getMessage();
+            exit;
+        }
+        $this->content = htmlspecialchars($user["content"]);
+    }
+
+
+    private function checkEmail($data) : string
+    {
+        if (filter_var($data, FILTER_VALIDATE_EMAIL)) {
+            return htmlspecialchars($data);
+        }
+        else {
+            throw new Exception('invalid mail address !');
+        }
+
+    }
 
     /**
      * @return string
