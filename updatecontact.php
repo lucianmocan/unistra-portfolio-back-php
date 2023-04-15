bb1669b45fa923edbc37421239e08e9daa2eb07e
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-15 12:16:53 +0200

    update contact by id

--- updatecontact.php
+++ updatecontact.php
@@ -1,1 +1,14 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+
+
+require_once "Contact.php";
+require_once "Post.php";
+
+$_POST = json_decode(file_get_contents("php://input"), true);
+if (isset($_POST)) {
+    $contact = new Contact($_POST);
+    $pdo = new Post();
+    $pdo->updateUserByID(intval($_POST['id'], 10), $contact);
+}
