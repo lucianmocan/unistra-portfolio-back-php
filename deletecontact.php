064e4525671e246d0cfacb59bd0b57be1f10cfe2
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-15 12:16:30 +0200

    deletecontact.php

--- deletecontact.php
+++ deletecontact.php
@@ -1,1 +1,14 @@
 <?php
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Headers: *');
+
+
+require_once "Contact.php";
+require_once "Post.php";
+
+$_POST = json_decode(trim(file_get_contents('php://input')),true);
+if (isset($_POST['params']['updates'][0]['value'])) {
+    $pdo = new Post();
+    $pdo->deleteUserByID($_POST['params']['updates'][0]['value']);
+    echo json_encode($_POST['params']['updates'][0]['value']);
+}
