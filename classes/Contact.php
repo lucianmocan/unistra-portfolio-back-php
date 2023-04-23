f5d3340f8a80dc610c73da686d448c0ac0eddff2
MOCAN LUCIAN <lucian.mocan@etu.unistra.fr>
2023-04-23 18:47:37 +0200

    data quality and quantity check

--- classes/Contact.php
+++ classes/Contact.php
@@ -7,24 +7,34 @@ class Contact
     private string $content;
 
     function __construct($user){
-        $this->firstName = htmlspecialchars($user["firstName"]);
-        $this->lastName = htmlspecialchars($user["lastName"]);
-        try {
-            $this->email = $this->checkEmail($user["email"]);
+        if (is_string($user["firstName"]) && is_string($user["lastName"])
+            && is_string($user["email"]) && is_string($user["content"])
+            && strlen($user["firstName"]) > 0 && strlen($user["firstName"]) <= 255
+            && strlen($user["lastName"]) > 0 && strlen($user["lastName"]) <= 255
+            && strlen($user["content"]) > 0 && strlen($user["content"]) <= 255) {
+                $this->firstName = htmlspecialchars($user["firstName"]);
+                $this->lastName = htmlspecialchars($user["lastName"]);
+                $this->content = htmlspecialchars($user["content"]);
+            try {
+                $this->email = $this->checkEmail($user["email"]);
+            } catch (Exception $e) {
+                http_response_code(406);
+                echo json_encode(array("message" => "incorrect email format"));
+                echo "Connection error" . $e->getMessage();
+                exit;
+            }
         }
-        catch (Exception $e) {
+        else {
             http_response_code(406);
-            echo json_encode(array("message" => "incorrect email format"));
-            echo "Connection error" . $e->getMessage();
+            echo json_encode(array("message" => "string size should be between 0 and 255"));
             exit;
         }
-        $this->content = htmlspecialchars($user["content"]);
     }
 
 
     private function checkEmail($data) : string
     {
-        if (filter_var($data, FILTER_VALIDATE_EMAIL)) {
+        if (filter_var($data, FILTER_VALIDATE_EMAIL) && strlen($data) <= 255) {
             return htmlspecialchars($data);
         }
         else {
